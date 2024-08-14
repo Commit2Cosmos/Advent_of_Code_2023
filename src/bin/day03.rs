368fdbda6c294aa2979b13c33bbdcd1113fe3001
Anton Belov <anton.belov.al7@gmail.com>
2024-08-14 14:04:14 +0100

    day03 solved

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -1,10 +1,13 @@
+use std::usize;
+
 use atoi::atoi;
 
 fn main() {
-    println!("Part 1: {}", part1());
-    // println!("Part 2: {}", part2());
+    // println!("Part 1: {}", part1());
+    println!("Part 2: {}", part2());
 }
 
+#[allow(dead_code)]
 fn part1() -> usize {
     let schematic = include_bytes!("../../input/day03_1.txt");
     let width = schematic.iter().position(|&x| x==b'\n').unwrap() as isize;
@@ -19,12 +22,12 @@ fn part1() -> usize {
         })
         //* find how many digits the number has */
         .map(|x| {
-            let end_idx = (x+1..=1+x+(width as usize-(x%width as usize)))
+            let length = (x+1..=1+x+(width as usize-(x%width as usize)))
                 .position(|i| !schematic.get(i).unwrap_or(&b' ').is_ascii_digit())
                 .unwrap_or(1+(width as usize-(x%width as usize))) + 1;
 
             //* return start idx and length */
-            (x, end_idx as isize)
+            (x, length as isize)
         })
         .filter(|(x, l)| {
             //* top row */
@@ -41,6 +44,46 @@ fn part1() -> usize {
             //* convert slice to number */
             atoi::<usize>(&schematic[x..x+e as usize]).unwrap()
         })
-        // .collect::<Vec<usize>>();
         .sum::<usize>()
+}
+
+
+fn part2() -> usize {
+    let schematic = include_bytes!("../../input/day03_2.txt");
+    let width = schematic.iter().position(|&x| x==b'\n').unwrap() as isize;
+    let mut nums = vec![];
+    
+    (0..schematic.len()).filter(|&i| schematic[i as usize] == b'*' )
+        //* find first indices of numbers */
+        .filter_map(|x| {
+            nums.clear();
+            nums.extend(
+                //* top row */
+                (-width-2..=-width)
+                //* left and right */
+                .chain([-1, 1])
+                //* bot row */
+                .chain(width..=width+2)
+                .map(|f| (x as isize+f) as usize)
+                //* check if is a digit */
+                .filter(|&f| schematic[f].is_ascii_digit())
+                //* find beginning of a number */
+                .filter_map(|j| {
+                    //* start from the digit found and go until reaches NaN */
+                    (j.saturating_sub(3)..=j)
+                    .rev()
+                    .take_while(|&c| schematic[c].is_ascii_digit())
+                    .last()
+                })
+            );
+            //* remove the same number if connects to '*' more than once */
+            nums.dedup();
+            (nums.len() == 2).then(|| {
+                nums.iter()
+                    .map(|&c| atoi::<usize>(&schematic[c..=c+3 as usize]).unwrap())
+                    .product::<usize>()
+            })
+        })
+        .sum::<usize>()
+
 }
